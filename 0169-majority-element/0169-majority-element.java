241cd03cdda0d1bb4b0a11e6d96085e085a4d9b6
Venkata Praneeth Gandi <praneethgandi99@gmail.com>
2026-05-23 10:03:29 +0530

    Time: 1 ms (99.86%), Space: 55.9 MB (6.56%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -2,18 +2,33 @@ class Solution {
     public int majorityElement(int[] nums) {
         
         // int count=0;
-        for(int i=0;i<nums.length;i++){
-           int count=0;
-            for(int j=0;j<nums.length;j++){
-                if(nums[i]==nums[j]){
-                    count+=1;
+        // for(int i=0;i<nums.length;i++){
+        //    int count=0;
+        //     for(int j=0;j<nums.length;j++){
+        //         if(nums[i]==nums[j]){
+        //             count+=1;
                     
-                }
+        //         }
+        //     }
+        //     if(count>nums.length/2){
+        //         return nums[i];
+        //     }
+        // }
+        // return -1;
+        int candidate=nums[0];
+        int points=1;
+        for(int i=1;i<nums.length;i++){
+            if(candidate==nums[i]){
+                points+=1;
             }
-            if(count>nums.length/2){
-                return nums[i];
+            else{
+                points-=1;
+            }
+            if(points<=0){
+                candidate=nums[i];
+                points=1;
             }
         }
-        return -1;
+        return candidate;
     }
 }
